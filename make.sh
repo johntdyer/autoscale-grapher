85e2ef1fbacb15ff639e5091b26a6795028d0b64
John Dyer <johntdyer@gmail.com>
2015-01-28 16:11:56 -0500

    finish build script

--- make.sh
+++ make.sh
@@ -8,22 +8,25 @@ go get github.com/tools/godep
 
 
 BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-VERSION=`cat version.go | grep "const Version string" | cut -d'"' -f2`
+VERSION=`cat VERSION`
 
 # Build for OSX
 godep go build -ldflags "-w -X main.buildDate ${BUILD_DATE} -X main.version ${VERSION}" -o builds/autoscale-grapher-$VERSION.osx
 if [ $? -eq 0 ]; then
-  echo "Success Build artifact - builds/tropo-grapher-$VERSION.osx"
+  echo "Success Build artifact - builds/autoscale-grapher-$VERSION.osx"
 else
   echo "Build error"
   exit $?
 fi
 
 # Build for Linux
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 godep go build -ldflags "-w -X main.buildDate ${BUILD_DATE} -X main.version ${VERSION}" -o builds/tropo-grapher-$VERSION.linux
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 godep go build -ldflags "-w -X main.buildDate ${BUILD_DATE} -X main.version ${VERSION}" -o builds/autoscale-grapher-$VERSION.linux
 if [ $? -eq 0 ]; then
-  echo "Success Build artifact - builds/tropo-grapher-$VERSION.linux"
+  echo "Success Build artifact - builds/autoscale-grapher-$VERSION.linux"
 else
   echo "Build error"
   exit $?
 fi
+
+chmod +x builds/autoscale-grapher-$VERSION.linux
+chmod +x builds/autoscale-grapher-$VERSION.osx
